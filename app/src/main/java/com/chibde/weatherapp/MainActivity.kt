e18896ddeea4e7278413863744f841084c8c0ea2
Gautam Chibde <gautamchibde@gmail.com>
2019-01-28 19:27:57 +0530

    init project with required dependencies

---
+++ app/src/main/java/com/chibde/weatherapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chibde.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
